5422087595353e34a8e008f6c09f91727aa79c19
Luiz Paulo Coutinho <coutinholps@gmail.com>
2023-12-11 16:55:15 -0300

    access_token

--- app/pages/🔑_Access_Token_BUENOSHOPS.py
+++ app/pages/🔑_Access_Token_BUENOSHOPS.py
@@ -34,36 +34,36 @@ st.write("Você digitou:", user_input_mcenter)
 # Cole aqui o url gerado após a autorização
 url_mcenter = user_input_mcenter
 
-match_mcenter = re.search(r"code=(.*)", url_mcenter)
+match_buenoshops = re.search(r"code=(.*)", url_mcenter)
 
-if match_mcenter:
-    code_mcenter = match_mcenter.group(1)
-    print(code_mcenter)
+if match_buenoshops:
+    code_buenoshops = match_buenoshops.group(1)
+    print(code_buenoshops)
 else:
     print("Nenhum valor encontrado entre 'code=' e '&state=' na string da URL.")
 
 ### Access Token ###
 url = "https://api.mercadolibre.com/oauth/token"
 
-payload_mcenter = f"grant_type=authorization_code&client_id={CLIENT_ID_BUENOSHOPS}&client_secret={SECRET_KEY_BUENOSHOPS}&code={code_mcenter}&redirect_uri={REDIRECT_URI_BUENOSHOPS}"
+payload_mcenter = f"grant_type=authorization_code&client_id={CLIENT_ID_BUENOSHOPS}&client_secret={SECRET_KEY_BUENOSHOPS}&code={code_buenoshops}&redirect_uri={REDIRECT_URI_BUENOSHOPS}"
 
 headers = {
     "accept": "application/json",
     "content-type": "application/x-www-form-urlencoded",
 }
 
-response_mcenter = requests.request("POST", url, headers=headers, data=payload_mcenter)
-print(response_mcenter.text)
+response_buenoshops = requests.request("POST", url, headers=headers, data=payload_mcenter)
+print(response_buenoshops.text)
 
-json_data_mcenter = response_mcenter.text
-data_mcenter = json.loads(json_data_mcenter)
+json_data_buenoshops = response_buenoshops.text
+data_buenoshops = json.loads(json_data_buenoshops)
 
 
-access_token_mcenter = data_mcenter.get("access_token")
-refresh_token_buenoshops = data_mcenter.get("refresh_token")
+access_token_buenoshops = data_buenoshops.get("access_token")
+refresh_token_buenoshops = data_buenoshops.get("refresh_token")
 
-if access_token_mcenter:
-    print("access_token_mcenter: ", access_token_mcenter)
+if access_token_buenoshops:
+    print("access_token_buenoshops: ", access_token_buenoshops)
 else:
     print("O campo 'access_token' não foi encontrado em nenhum JSON.")
 
@@ -98,13 +98,13 @@ with open(env_path, "r") as file:
 
 # Atualiza ACCESS_TOKEN e REFRESH_TOKEN
 for i in range(len(lines)):
-    if lines[i].startswith("ACCESS_TOKEN_MCENTER="):
-        lines[i] = f"ACCESS_TOKEN_MCENTER='{access_token_mcenter}'\n"
+    if lines[i].startswith("ACCESS_TOKEN_BUENOSHOPS="):
+        lines[i] = f"ACCESS_TOKEN_BUENOSHOPS='{access_token_mcenter}'\n"
     elif lines[i].startswith("REFRESH_TOKEN_BUENOSHOPS="):
         lines[i] = f"REFRESH_TOKEN_BUENOSHOPS='{refresh_token_buenoshops}'\n"
     
 with open(env_path, "w") as file:
     file.writelines(lines)
 
-st.write("access_token_mcenter:", access_token_mcenter)
+st.write("ACCESS_TOKEN_BUENOSHOPS:", access_token_mcenter)
 st.write("REFRESH_TOKEN_BUENOSHOPS:", refresh_token_buenoshops)
