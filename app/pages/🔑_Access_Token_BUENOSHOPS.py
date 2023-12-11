5ccfa1eccdcc4639a99c55c0cc2a01e20494e91f
Luiz Paulo Coutinho <coutinholps@gmail.com>
2023-12-11 16:35:06 -0300

    fix buenoshops

--- app/pages/🔑_Access_Token_BUENOSHOPS.py
+++ app/pages/🔑_Access_Token_BUENOSHOPS.py
@@ -16,54 +16,54 @@ CLIENT_ID_BUENOSHOPS = os.getenv("CLIENT_ID_BUENOSHOPS")
 SECRET_KEY_BUENOSHOPS = os.getenv("SECRET_KEY_BUENOSHOPS")
 REDIRECT_URI_BUENOSHOPS = os.getenv("REDIRECT_URI_BUENOSHOPS")
 
-
-auth_link_buenoshops = f"https://auth.mercadolivre.com.br/authorization?response_type=code&client_id={CLIENT_ID_BUENOSHOPS}&redirect_uri={REDIRECT_URI_BUENOSHOPS}"
+st.header("Capturando Access Token do ML")
+auth_link_mcenter = f"https://auth.mercadolivre.com.br/authorization?response_type=code&client_id={CLIENT_ID_BUENOSHOPS}&redirect_uri={REDIRECT_URI_BUENOSHOPS}"
 
 f"""
 Clique no link: 
 
-{auth_link_buenoshops}
+{auth_link_mcenter}
 """
 
-user_input_buenoshops = st.text_input("Digite a url gerado a partir do link de Buenos Shop:")
-
+# Adiciona um campo de entrada de texto no Streamlit
+user_input_mcenter = st.text_input("Digite a url gerado a partir do link:")
 
 # Exibe o texto inserido pelo usuário
-st.write("Você digitou:", user_input_buenoshops)
+st.write("Você digitou:", user_input_mcenter)
 
-url_buenoshops = user_input_buenoshops
+# Cole aqui o url gerado após a autorização
+url_mcenter = user_input_mcenter
 
-match_buenoshops = re.search(r"code=(.*)", url_buenoshops)
+match_mcenter = re.search(r"code=(.*)", url_mcenter)
 
-if match_buenoshops:
-    code_buenoshops = match_buenoshops.group(1)
-    print(code_buenoshops)
+if match_mcenter:
+    code_mcenter = match_mcenter.group(1)
+    print(code_mcenter)
 else:
     print("Nenhum valor encontrado entre 'code=' e '&state=' na string da URL.")
 
 ### Access Token ###
 url = "https://api.mercadolibre.com/oauth/token"
 
-payload_buenoshops = f"grant_type=authorization_code&client_id={CLIENT_ID_BUENOSHOPS}&client_secret={SECRET_KEY_BUENOSHOPS}&code={code_buenoshops}&redirect_uri={REDIRECT_URI_BUENOSHOPS}"
+payload_mcenter = f"grant_type=authorization_code&client_id={CLIENT_ID_BUENOSHOPS}&client_secret={SECRET_KEY_BUENOSHOPS}&code={code_mcenter}&redirect_uri={REDIRECT_URI_BUENOSHOPS}"
 
 headers = {
     "accept": "application/json",
     "content-type": "application/x-www-form-urlencoded",
 }
 
-response_buenoshops = requests.request("POST", url, headers=headers, data=payload_buenoshops)
+response_mcenter = requests.request("POST", url, headers=headers, data=payload_mcenter)
+print(response_mcenter.text)
 
-print(response_buenoshops.text)
-
-json_data_buenoshops = response_buenoshops.text
-data_buenoshops = json.loads(json_data_buenoshops)
-
-access_token_buenoshops = data_buenoshops.get("access_token")
-refresh_token_buenoshops = data_buenoshops.get("refresh_token")
+json_data_mcenter = response_mcenter.text
+data_mcenter = json.loads(json_data_mcenter)
 
 
-if access_token_buenoshops:
-    print("access_token_buenoshops: ", access_token_buenoshops)
+access_token_mcenter = data_mcenter.get("access_token")
+refresh_token_buenoshops = data_mcenter.get("refresh_token")
+
+if access_token_mcenter:
+    print("access_token_mcenter: ", access_token_mcenter)
 else:
     print("O campo 'access_token' não foi encontrado em nenhum JSON.")
 
@@ -98,13 +98,13 @@ with open(env_path, "r") as file:
 
 # Atualiza ACCESS_TOKEN e REFRESH_TOKEN
 for i in range(len(lines)):
-    if lines[i].startswith("ACCESS_TOKEN_BUENOSHOPS="):
-        lines[i] = f"ACCESS_TOKEN_BUENOSHOPS='{access_token_buenoshops}'\n"
+    if lines[i].startswith("ACCESS_TOKEN_MCENTER="):
+        lines[i] = f"ACCESS_TOKEN_MCENTER='{access_token_mcenter}'\n"
     elif lines[i].startswith("REFRESH_TOKEN_BUENOSHOPS="):
         lines[i] = f"REFRESH_TOKEN_BUENOSHOPS='{refresh_token_buenoshops}'\n"
-
+    
 with open(env_path, "w") as file:
     file.writelines(lines)
 
-st.write("access_token_buenoshops", access_token_buenoshops)
-st.write("refresh_token_buenoshops:", refresh_token_buenoshops)
+st.write("access_token_mcenter:", access_token_mcenter)
+st.write("REFRESH_TOKEN_BUENOSHOPS:", refresh_token_buenoshops)
