5d4cf2055baa9ae1ba9a742c81766dc848757e62
Luiz Paulo Coutinho <coutinholps@gmail.com>
2023-12-11 15:28:46 -0300

    access_token para musicalcris e buenoshops

--- app/1_🔑_Access_Token_ML.py
+++ app/1_🔑_Access_Token_ML.py
@@ -11,113 +11,180 @@ from dotenv import find_dotenv, load_dotenv
 load_dotenv()
 # load_dotenv(find_dotenv())
 
-CLIENT_ID = os.getenv("CLIENT_ID")
-SCRET_KEY = os.getenv("SCRET_KEY")
-REDIRECT_URI = os.getenv("REDIRECT_URI")
-
-start_prog = time.time()
-
-clientId = CLIENT_ID
-secretKey = SCRET_KEY
-redirectURI = REDIRECT_URI
+REFRESH_TOKEN_MCENTER = os.getenv("REFRESH_TOKEN_MCENTER")
+CLIENT_ID_MCENTER = os.getenv("CLIENT_ID_MCENTER")
+SECRET_KEY_MCENTER = os.getenv("SECRET_KEY_MCENTER")
+REDIRECT_URI_MCENTER = os.getenv("REDIRECT_URI_MCENTER")
+REFRESH_TOKEN_BUENOSHOPS = os.getenv("REFRESH_TOKEN_BUENOSHOPS")
+CLIENT_ID_BUENOSHOPS = os.getenv("CLIENT_ID_BUENOSHOPS")
+SECRET_KEY_BUENOSHOPS = os.getenv("SECRET_KEY_BUENOSHOPS")
+REDIRECT_URI_BUENOSHOPS = os.getenv("REDIRECT_URI_BUENOSHOPS")
+REFRESH_TOKEN_MUSICALCRIS = os.getenv("REFRESH_TOKEN_MUSICALCRIS")
+CLIENT_ID_MUSICALCRIS = os.getenv("CLIENT_ID_MUSICALCRIS")
+SECRET_KEY_MUSICALCRIS = os.getenv("SECRET_KEY_MUSICALCRIS")
+REDIRECT_URI_MUSICALCRIS = os.getenv("REDIRECT_URI_MUSICALCRIS")
 
 st.header("Capturando Access Token do ML")
-auth_link = f"https://auth.mercadolivre.com.br/authorization?response_type=code&client_id={clientId}&redirect_uri={redirectURI}"
+auth_link_mcenter = f"https://auth.mercadolivre.com.br/authorization?response_type=code&client_id={CLIENT_ID_MCENTER}&redirect_uri={REDIRECT_URI_MCENTER}"
 
 f"""
-Clique no link {auth_link}
+Clique no link: 
+
+{auth_link_mcenter}
 """
 
 # Adiciona um campo de entrada de texto no Streamlit
-user_input = st.text_input("Digite a url gerado a partir do link:")
+user_input_mcenter = st.text_input("Digite a url gerado a partir do link:")
+
+auth_link_buenoshops = f"https://auth.mercadolivre.com.br/authorization?response_type=code&client_id={CLIENT_ID_BUENOSHOPS}&redirect_uri={REDIRECT_URI_BUENOSHOPS}"
+
+f"""
+Clique no link: 
+
+{auth_link_buenoshops}
+"""
+
+user_input_buenoshops = st.text_input("Digite a url gerado a partir do link de Buenos Shop:")
+
+auth_link_musicalcris = f"https://auth.mercadolivre.com.br/authorization?response_type=code&client_id={CLIENT_ID_MUSICALCRIS}&redirect_uri={REDIRECT_URI_MUSICALCRIS}"
+
+f"""
+Clique no link: 
+
+{auth_link_musicalcris}
+"""
+
+user_input_musicalcris = st.text_input("Digite a url gerado a partir de Musical Cris:")
+
 
 # Exibe o texto inserido pelo usuário
-st.write("Você digitou:", user_input)
+st.write("Você digitou:", user_input_mcenter)
 
 # Cole aqui o url gerado após a autorização
-url = user_input
+url_mcenter = user_input_mcenter
+url_buenoshops = user_input_buenoshops
+url_musicalcris = user_input_musicalcris
 
-match = re.search(r"code=(.*)", url)
+match_mcenter = re.search(r"code=(.*)", url_mcenter)
+match_buenoshops = re.search(r"code=(.*)", url_buenoshops)
+match_musicalcris = re.search(r"code=(.*)", url_musicalcris)
 
-if match:
-    code = match.group(1)
-    print(code)
+if match_mcenter:
+    code_mcenter = match_mcenter.group(1)
+    print(code_mcenter)
+elif match_buenoshops:
+    code_buenoshops = match_buenoshops.group(1)
+    print(code_buenoshops)
+elif match_musicalcris:
+    code_musicalcris = match_musicalcris.group(1)
+    print(code_musicalcris)
 else:
     print("Nenhum valor encontrado entre 'code=' e '&state=' na string da URL.")
 
 ### Access Token ###
 url = "https://api.mercadolibre.com/oauth/token"
 
-payload = f"grant_type=authorization_code&client_id={clientId}&client_secret={secretKey}&code={code}&redirect_uri={redirectURI}"
+payload_mcenter = f"grant_type=authorization_code&client_id={CLIENT_ID_MCENTER}&client_secret={SECRET_KEY_MCENTER}&code={code_mcenter}&redirect_uri={REDIRECT_URI_MCENTER}"
+payload_buenoshops = f"grant_type=authorization_code&client_id={CLIENT_ID_BUENOSHOPS}&client_secret={SECRET_KEY_BUENOSHOPS}&code={code_buenoshops}&redirect_uri={REDIRECT_URI_BUENOSHOPS}"
+payload_musicalcris = f"grant_type=authorization_code&client_id={CLIENT_ID_MUSICALCRIS}&client_secret={SECRET_KEY_MUSICALCRIS}&code={code_musicalcris}&redirect_uri={REDIRECT_URI_MUSICALCRIS}"
+
 headers = {
     "accept": "application/json",
     "content-type": "application/x-www-form-urlencoded",
 }
 
-response = requests.request("POST", url, headers=headers, data=payload)
+response_mcenter = requests.request("POST", url, headers=headers, data=payload_mcenter)
+response_buenoshops = requests.request("POST", url, headers=headers, data=payload_buenoshops)
+response_musicalcris = requests.request("POST", url, headers=headers, data=payload_musicalcris)
 
-print(response.text)
+print(response_mcenter.text)
+print(response_buenoshops.text)
+print(response_musicalcris.text)
 
-json_data = response.text
-data = json.loads(json_data)
+json_data_mcenter = response_mcenter.text
+data_mcenter = json.loads(json_data_mcenter)
 
-access_token = data.get("access_token")
-refresh_token = data.get("refresh_token")
+json_data_buenoshops = response_buenoshops.text
+data_buenoshops = json.loads(json_data_buenoshops)
+
+json_data_musicalcris = response_musicalcris.text
+data_musicalcris = json.loads(json_data_musicalcris)
+
+access_token_mcenter = data_mcenter.get("access_token")
+refresh_token_mcenter = data_mcenter.get("refresh_token")
+
+access_token_buenoshops = data_buenoshops.get("access_token")
+refresh_token_buenoshops = data_buenoshops.get("refresh_token")
+
+access_token_musicalcris = data_musicalcris.get("access_token")
+refresh_token_musicalcris = data_musicalcris.get("refresh_token")
 
 
-if access_token:
-    print("access_token: ", access_token)
+if access_token_mcenter:
+    print("access_token_mcenter: ", access_token_mcenter)
+if access_token_buenoshops:
+    print("access_token_buenoshops: ", access_token_buenoshops)
+if access_token_musicalcris:
+    print("access_token_musicalcris: ", access_token_musicalcris)
 else:
-    print("O campo 'access_token' não foi encontrado no JSON.")
+    print("O campo 'access_token' não foi encontrado em nenhum JSON.")
 
-if refresh_token:
-    print("refresh_token: ", refresh_token)
+if refresh_token_mcenter:
+    print("refresh_token_mcenter: ", refresh_token_mcenter)
+if refresh_token_buenoshops:
+    print("refresh_token_buenoshops: ", refresh_token_buenoshops)
+if refresh_token_musicalcris:
+    print("refresh_token_musicalcris: ", refresh_token_musicalcris)
 else:
-    print("O campo 'refresh_token' não foi encontrado no JSON.")
-
-### Refresh token ###
-url = "https://api.mercadolibre.com/oauth/token"
-payload = f"grant_type=refresh_token&client_id={clientId}&client_secret={secretKey}&refresh_token={refresh_token}"
-headers = {
-    "accept": "application/json",
-    "content-type": "application/x-www-form-urlencoded",
-}
-
-response = requests.request("POST", url, headers=headers, data=payload)
-
-print(response.text)
-
-json_data = response.text
-data = json.loads(json_data)
-
-access_token_ = data.get("access_token")
-refresh_token_ = data.get("refresh_token")
+    print("O campo 'refresh_token' não foi encontrado em nenhum no JSON.")
 
 
-### Atualiza o arquivo .env com o novo ACCESS_TOKEN ###
+# ### Refresh token ###
+# url = "https://api.mercadolibre.com/oauth/token"
+# payload = f"grant_type=refresh_token&client_id={clientId}&client_secret={secretKey}&refresh_token={refresh_token}"
+# headers = {
+#     "accept": "application/json",
+#     "content-type": "application/x-www-form-urlencoded",
+# }
+
+# response = requests.request("POST", url, headers=headers, data=payload)
+
+# print(response.text)
+
+# json_data = response.text
+# data = json.loads(json_data)
+
+# access_token_ = data.get("access_token")
+# refresh_token_ = data.get("refresh_token")
+
 env_path = find_dotenv()
+
 with open(env_path, "r") as file:
-    lines = file.readlines()
+            lines = file.readlines()
+
+# Atualiza ACCESS_TOKEN e REFRESH_TOKEN
+for i in range(len(lines)):
+    if lines[i].startswith("ACCESS_TOKEN_MCENTER="):
+        lines[i] = f"ACCESS_TOKEN_MCENTER='{access_token_mcenter}'\n"
+    elif lines[i].startswith("REFRESH_TOKEN_MCENTER="):
+        lines[i] = f"REFRESH_TOKEN_MCENTER='{refresh_token_mcenter}'\n"
+    if lines[i].startswith("ACCESS_TOKEN_BUENOSHOPS="):
+        lines[i] = f"ACCESS_TOKEN_BUENOSHOPS='{access_token_buenoshops}'\n"
+    elif lines[i].startswith("REFRESH_TOKEN_BUENOSHOPS="):
+        lines[i] = f"REFRESH_TOKEN_BUENOSHOPS='{refresh_token_buenoshops}'\n"
+    if lines[i].startswith("ACCESS_TOKEN_MUSICALCRIS="):
+        lines[i] = f"ACCESS_TOKEN_MUSICALCRIS='{access_token_musicalcris}'\n"
+    elif lines[i].startswith("REFRESH_TOKEN_MUSICALCRIS="):
+        lines[i] = f"REFRESH_TOKEN_MUSICALCRIS='{refresh_token_musicalcris}'\n"
 
 with open(env_path, "w") as file:
-    for line in lines:
-        if line.startswith("ACCESS_TOKEN="):
-            line = f"ACCESS_TOKEN='{access_token}'\n"
-        file.write(line)
+    file.writelines(lines)
 
-### Atualiza o arquivo .env com o novo REFRESH_TOKEN ###
-env_path = find_dotenv()
-with open(env_path, "r") as file:
-    lines = file.readlines()
+st.write("access_token_mcenter:", access_token_mcenter)
+st.write("refresh_token_mcenter:", refresh_token_mcenter)
 
-with open(env_path, "w") as file:
-    for line in lines:
-        if line.startswith("REFRESH_TOKEN="):
-            line = f"REFRESH_TOKEN='{refresh_token}'\n"
-        file.write(line)
+st.write("access_token_buenoshops", access_token_buenoshops)
+st.write("refresh_token_buenoshops:", refresh_token_buenoshops)
 
-st.write("access_token:", access_token)
-st.write("refresh_token:", refresh_token)
-
-st.write("access_token:", access_token_)
-st.write("refresh_token:", refresh_token_)
+st.write("access_token_musicalcris", access_token_musicalcris)
+st.write("refresh_token_musicalcris:", refresh_token_musicalcris)
--- ml_consume/auth.py
+++ ml_consume/auth.py
@@ -5,6 +5,19 @@ import requests
 from dotenv import find_dotenv, load_dotenv
 from loguru import logger
 
+load_dotenv()
+
+REFRESH_TOKEN_MCENTER = os.getenv("REFRESH_TOKEN_MCENTER")
+CLIENT_ID_MCENTER = os.getenv("CLIENT_ID_MCENTER")
+SECRET_KEY_MCENTER = os.getenv("SECRET_KEY_MCENTER")
+REFRESH_TOKEN_BUENOSHOPS = os.getenv("REFRESH_TOKEN_BUENOSHOPS")
+CLIENT_ID_BUENOSHOPS = os.getenv("CLIENT_ID_BUENOSHOPS")
+SECRET_KEY_BUENOSHOPS = os.getenv("SECRET_KEY_BUENOSHOPS")
+REFRESH_TOKEN_MUSICALCRIS = os.getenv("REFRESH_TOKEN_MUSICALCRIS")
+CLIENT_ID_MUSICALCRIS = os.getenv("CLIENT_ID_MUSICALCRIS")
+SECRET_KEY_MUSICALCRIS = os.getenv("SECRET_KEY_MUSICALCRIS")
+
+
 def update_tokens(env_path, access_token, refresh_token):
     logger.info("Atualizando ACCESS_TOKEN e REFRESH_TOKEN")
 
@@ -17,18 +30,26 @@ def update_tokens(env_path, access_token, refresh_token):
 
     # Atualiza ACCESS_TOKEN e REFRESH_TOKEN
     for i in range(len(lines)):
-        if lines[i].startswith("ACCESS_TOKEN="):
-            lines[i] = f"ACCESS_TOKEN='{access_token}'\n"
-        elif lines[i].startswith("REFRESH_TOKEN="):
-            lines[i] = f"REFRESH_TOKEN='{refresh_token}'\n"
-
+        if lines[i].startswith("ACCESS_TOKEN_MCENTER="):
+            lines[i] = f"ACCESS_TOKEN_MCENTER='{access_token}'\n"
+        elif lines[i].startswith("REFRESH_TOKEN_MCENTER="):
+            lines[i] = f"REFRESH_TOKEN_MCENTER='{refresh_token}'\n"
+        if lines[i].startswith("ACCESS_TOKEN_BUENOSHOPS="):
+            lines[i] = f"ACCESS_TOKEN_BUENOSHOPS='{access_token}'\n"
+        elif lines[i].startswith("REFRESH_TOKEN_BUENOSHOPS="):
+            lines[i] = f"REFRESH_TOKEN_BUENOSHOPS='{refresh_token}'\n"
+        if lines[i].startswith("ACCESS_TOKEN_MUSICALCRIS="):
+            lines[i] = f"ACCESS_TOKEN_MUSICALCRIS='{access_token}'\n"
+        elif lines[i].startswith("REFRESH_TOKEN_MUSICALCRIS="):
+            lines[i] = f"REFRESH_TOKEN_MUSICALCRIS='{refresh_token}'\n"
+                
     with open(env_path, "w") as file:
         file.writelines(lines)
 
     logger.info("Tokens atualizados com sucesso.")
 
 
-def refresh_tokens():
+def refresh_tokens(CLIENT_ID,SECRET_KEY,REFRESH_TOKEN):
     try:
         logger.add(
             "Data/Output/Log/auth.log",
@@ -36,12 +57,12 @@ def refresh_tokens():
             format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
         )
 
-        load_dotenv()
+        # load_dotenv()
 
-        CLIENT_ID = os.getenv("CLIENT_ID")
-        SCRET_KEY = os.getenv("SCRET_KEY")
-        REDIRECT_URI = os.getenv("REDIRECT_URI")
-        REFRESH_TOKEN = os.getenv("REFRESH_TOKEN")
+        # CLIENT_ID = os.getenv("CLIENT_ID")
+        # SCRET_KEY = os.getenv("SCRET_KEY")
+        # REDIRECT_URI = os.getenv("REDIRECT_URI")
+        # REFRESH_TOKEN = os.getenv("REFRESH_TOKEN")
 
         logger.info("Gerando novo access_token e refresh_token")
 
@@ -67,12 +88,41 @@ def refresh_tokens():
 
         env_path = find_dotenv()
         
-        update_tokens(env_path, access_token, refresh_token)
+        logger.info("Atualizando ACCESS_TOKEN e REFRESH_TOKEN")
 
+        if not os.path.exists(env_path):
+            logger.error(f"Arquivo de configurações não encontrado.")
+            return
+
+        with open(env_path, "r") as file:
+            lines = file.readlines()
+
+        # Atualiza ACCESS_TOKEN e REFRESH_TOKEN
+        for i in range(len(lines)):
+            if lines[i].startswith("ACCESS_TOKEN_MCENTER="):
+                lines[i] = f"ACCESS_TOKEN_MCENTER='{access_token}'\n"
+            elif lines[i].startswith("REFRESH_TOKEN_MCENTER="):
+                lines[i] = f"REFRESH_TOKEN_MCENTER='{refresh_token}'\n"
+            if lines[i].startswith("ACCESS_TOKEN_BUENOSHOPS="):
+                lines[i] = f"ACCESS_TOKEN_BUENOSHOPS='{access_token}'\n"
+            elif lines[i].startswith("REFRESH_TOKEN_BUENOSHOPS="):
+                lines[i] = f"REFRESH_TOKEN_BUENOSHOPS='{refresh_token}'\n"
+            if lines[i].startswith("ACCESS_TOKEN_MUSICALCRIS="):
+                lines[i] = f"ACCESS_TOKEN_MUSICALCRIS='{access_token}'\n"
+            elif lines[i].startswith("REFRESH_TOKEN_MUSICALCRIS="):
+                lines[i] = f"REFRESH_TOKEN_MUSICALCRIS='{refresh_token}'\n"
+                    
+        with open(env_path, "w") as file:
+            file.writelines(lines)
+
+        logger.info("Tokens atualizados com sucesso.")
+            
     except Exception as e:
         logger.error(f"Error: {str(e)}")
 
     logger.info(f"Novo access_token: {access_token}")
     logger.info(f"Novo refresh_token: {refresh_token}")
 
-refresh_tokens()
+refresh_tokens(CLIENT_ID_MCENTER,SECRET_KEY_MCENTER,REFRESH_TOKEN_MCENTER)
+refresh_tokens(CLIENT_ID_BUENOSHOPS,SECRET_KEY_BUENOSHOPS,REFRESH_TOKEN_BUENOSHOPS)
+refresh_tokens(CLIENT_ID_MUSICALCRIS,SECRET_KEY_MUSICALCRIS,REFRESH_TOKEN_MUSICALCRIS)
